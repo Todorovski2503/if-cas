8be3f51ff6e49dd41a807dd209514619bbf7ac38
Todorovski2503 <113542860+Todorovski2503@users.noreply.github.com>
2022-09-14 19:03:02 +0200

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+let god=Number(prompt("Unesite godine"));
+if (Number.isNaN(god)){
+  console.log("Niste uneli broj!");
+}else{
+if(god>=18){
+  console.log("Punoletni ste");
+}else{
+  console.log("Niste punoletni");
+}
+}
